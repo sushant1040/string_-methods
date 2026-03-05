fab63a9d313f681c1b6fc714057aa141a64edab9
sushant1040 <use.sushantkumar@gmail.com>
2026-03-05 20:08:55 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,142 @@
+# All the string method
+# string.capitalize
+
+# string= "i am sushant"
+
+# string1=string.capitalize()
+
+# print(string1)
+# I am sushant output
+
+#
+
+# string= "I am sushant"
+# string1=string.casefold()
+# print(string1)
+# i am sushant output 
+# string="Banana"
+# string1=string.center(20)
+# print(string1)
+# #        Banana   output make space 
+
+# string= "I am sushant and i am sushant friend"
+# string1=string.count("sushant")
+# print(string1)
+# 2 output
+
+
+# string= "i am sushant"
+
+# string1=string.encode()
+# print(string1)
+# b'i am sushant' output
+
+# string= "i am sushant "
+
+# string1=string.endswith(" ")
+# print(string1)
+# output will in boolen value in this case it give True
+
+# txt = "H\te\tl\tl\to"
+
+# x =  txt.expandtabs(2)
+
+# print(x)
+# having doubt
+
+# string= "i am sushant"
+
+# string1=string.find("sushant")
+# print(string1)
+# having doubt
+
+# string= "i am sushant"
+
+# string1=string.format()
+# print(string1)
+# no idea
+
+# string= "i am sushant"
+
+# string1=string.index("sushant")
+# print(string1)
+# 5 output
+
+# string= "i am sushant "
+
+# string1=string.isalnum()
+# print(string1)
+# not sure
+
+# string= "iamsushant"
+
+# string1=string.isalpha()
+# print(string1)
+# output This method checks only alphabet should in string
+
+# string= "iamsushant"
+
+# string1=string.isidentifier()
+# print(string1)
+# In this case output will be true
+
+# string= "i am sushant"
+
+# string1=string.isidentifier()
+# print(string1)
+# In this case the output only false because of space 
+
+# string= "I AM SUSHANT"
+
+# string1=string.islower()
+# print(string1)
+# in this case output will be false this method Shows the string is lower or  not
+
+# string= "I AM SUSHANT"
+
+# string1=string.isprintable()
+# print(string1)
+# having doubt
+# string= " "
+
+# string1=string.isspace()
+# print(string1)
+# output check is space exist in string
+
+# string= "sushant"
+
+# string1=string.isupper()
+# print(string1)
+# this check string is uppercase or not.
+
+# string= "sushant"
+
+# string1=string.join()
+# print(string1)
+
+# doubt
+
+# string= "sushant"
+
+# string1=string.ljust(34)
+# print(string1,'i live at delhi')
+# output for makes spaces between strings
+
+# string= "SUSHANT "
+
+# string1=string.lower()
+# print(string1)
+# output sushant
+
+# string= "SUSHANT "
+
+# string1=string.lstrip()
+# print("I AM ",string1)
+# output inject string in paragraph
+
+# string= "sushant and rohit"
+
+# string1=string.replace("rohit","mohit")
+# print(string1)
+# output sushant and mohit
+
